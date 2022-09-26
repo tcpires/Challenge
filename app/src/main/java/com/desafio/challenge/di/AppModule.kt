d06b87aee772ff8478731759a80d497a896ce9b3
tcpires <thiago.pires@gmail.com>
2022-09-26 23:17:02 +0300

    configure catalog version to gradlew

--- app/src/main/java/com/desafio/challenge/di/AppModule.kt
+++ app/src/main/java/com/desafio/challenge/di/AppModule.kt
@@ -1,10 +1,9 @@
 package com.desafio.challenge.di
 
-import com.desafio.data.remote.config.JacksonConfig
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 
 val appModule = module {
-    single<String>(named(name = Properties.BASE_URL)){getProperty(Properties.BASE_URL)}
+    single<String>(named(name = Properties.BASE_URL)) { getProperty(Properties.BASE_URL) }
 }
